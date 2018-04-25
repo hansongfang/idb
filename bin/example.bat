8a6d93353358e5c29d67ceb831a63e99daf0b4a3
Songfang <shanaf@ust.hk>
2018-04-25 13:30:59 +0800

    fix bat

--- bin/example.bat
+++ bin/example.bat
@@ -1,14 +1,14 @@
-rem dag-merger.exe -m "../PLY/bunny1k.ply" -s 2 -l 1 -e 1000.0 -c "./debug" -p 1 -i 5
-rem dag-viewer.exe -m "../PLY/bunny1k.ply" -c "./debug/bunny1k/2_1/1000_near3/"
+dag-merger.exe -m "../PLY/bunny1k.ply" -s 2 -l 1 -e 1000.0 -c "./debug" -p 1 -i 5
+dag-viewer.exe -m "../PLY/bunny1k.ply" -c "./debug/bunny1k/2_1/1000_near3/"
 
 dag-merger.exe -m "../PLY/bunny10k.ply" -s 2 -l 1 -e 1000.0 -c "./debug" -p 1 -i 5
 dag-viewer.exe -m "../PLY/bunny10k.ply" -c "./debug/bunny10k/2_1/1000_near3/"
 
-rem dag-upsample.exe -m "../PLY/bunny5k.ply" -n "../PLY/bunny1k.ply" -s 2 -l 1 -e 1000.0 -c "./debug" -b "./debug"
-rem dag-viewer.exe -m "../PLY/bunny5k.ply" -c "./debug/bunny5k/2_1/1000_near3/"
+dag-upsample.exe -m "../PLY/bunny5k.ply" -n "../PLY/bunny1k.ply" -s 2 -l 1 -e 1000.0 -c "./debug" -b "./debug"
+dag-viewer.exe -m "../PLY/bunny5k.ply" -c "./debug/bunny5k/2_1/1000_near3/"
 
 dag-upsample.exe -m "../PLY/bunny30k.ply" -n "../PLY/bunny10k.ply" -s 2 -l 1 -e 1000.0 -c "./debug" -b "./debug"
-dag-viewer.exe -m "../PLY/bunny30k.ply" -c "./debug/bunny10k/2_1/1000_near3/"
+dag-viewer.exe -m "../PLY/bunny30k.ply" -c "./debug/bunny30k/2_1/1000_near3/"
 
 pause
 
